2dc96b6477309f8a9df3f4095d3d78c4109f6254
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-27 17:11:55 +0100

    Show sources of repos with logos

--- web/views/cb_view.ex
+++ web/views/cb_view.ex
@@ -1,16 +1,8 @@
 defmodule HelloPhoenix.CodebaseHQView do
   use HelloPhoenix.Web, :view
 
-  def id do
-    elem(Github.getOrg,1)["id"]
-  end
-
   def repos do
     Source.get(:codebaseHQ, :repos)
-  end
-
-  def tickets do
-
   end
 
   def bugs(permalink) do
