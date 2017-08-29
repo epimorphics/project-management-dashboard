daaf66698dea672dd817bbb28722f0aec2a3895b
Max Prettyjohns <heshoots9999@gmail.com>
2017-08-29 14:21:00 +0100

    Save transform as base64 string

--- web/models/Fuseki.ex
+++ web/models/Fuseki.ex
@@ -180,7 +180,9 @@ defmodule Fuseki do
   end
 
   def getProject(name) do
-    [%{:name => name, :repos => [], :trello => []}]
+    queryDB("SELECT ?url ?transform WHERE { ?project rdf:type :project . ?project :transform ?transform . ?project rdf:resource ?url . ?project rdf:name \"" <> name <> "\" . }")
+    |> parseJSON
+    |> Enum.reduce([], fn(x, all) -> all ++  [%{:name => name, :transform => Base.decode64!(x["transform"]), :url => x["url"], :repos => [], :trello => []}] end)
     |> getRepos
     |> getTrello
     |> List.first
@@ -204,9 +206,9 @@ defmodule Fuseki do
   end
 
   def newProjects do
-    queryDB("SELECT ?name ?url WHERE { ?project rdf:type :project. ?project rdf:resource ?url . ?project rdf:name ?name. }")
+    queryDB("SELECT ?name ?url ?transform WHERE { ?project rdf:type :project. ?project :transform ?transform . ?project rdf:resource ?url . ?project rdf:name ?name. }")
     |> parseJSON
-    |> Enum.reduce([], fn(x, all) -> all ++  [%{:name => x["name"], :url => x["url"], :repos => [], :trello => []}] end)
+    |> Enum.reduce([], fn(x, all) -> all ++  [%{:name => x["name"], :transform => Base.decode64!(x["transform"]), :url => x["url"], :repos => [], :trello => []}] end)
     |> getRepos
     |> getTrello
   end
@@ -219,6 +221,7 @@ defmodule Fuseki do
     "INSERT { "<>
     "_:project rdf:type :project . " <>
     "_:project rdf:name \"" <> project["name"] <> "\" . " <>
+    "_:project :transform \"" <> Base.encode64(project["transform"]) <> "\" . " <>
     "_:project rdf:resource <http://localhost:8080/#/project?name=" <> URI.encode(project["name"]) <> "> . " <>
     " } WHERE {} ; " <>
     Enum.reduce(project["repos"], "", fn(x, all) ->
